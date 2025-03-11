26defec3faffa86ee247903aa4a8545ebf2f05bf
Syed Muhammad Athar <786pub@gmail.com>
2025-03-11 23:23:45 +0500

    Initial project upload

---
+++ project/frontend/src/pages/Services.jsx
@@ -0,0 +1,78 @@
+
+export default function Services() {
+    return (
+        <>
+            <div className="container relative flex flex-col justify-between h-full max-w-6xl px-10 mx-auto xl:px-0 mt-5">
+                <h2 className="mb-1 text-3xl font-extrabold leading-tight text-white-900">Services</h2>
+                <p className="mb-12 text-lg text-white-500">Here is a few of the awesome Services we provide.</p>
+                <div className="w-full">
+                    <div className="flex flex-col w-full mb-10 sm:flex-row">
+                        <div className="w-full mb-10 sm:mb-0 sm:w-1/2">
+                            <div className="relative h-full ml-0 mr-0 sm:mr-10">
+                                <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-indigo-500 rounded-lg"></span>
+                                <div className="relative h-full p-5 bg-white border-2 border-indigo-500 rounded-lg">
+                                    <div className="flex items-center -mt-1">
+                                        <h3 className="my-2 ml-3 text-lg font-bold text-gray-800">Free Delivery</h3>
+                                    </div>
+                                    <p className="mt-3 mb-1 text-xs font-medium text-indigo-500 uppercase">------------</p>
+                                    <p className="mb-2 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum voluptates quibusdam, beatae dolorem temporibus ullam excepturi hic cupiditate id asperiores sint omnis tempore! Quidem harum incidunt eaque repellendus distinctio vero!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full sm:w-1/2">
+                            <div className="relative h-full ml-0 md:mr-10">
+                                <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-purple-500 rounded-lg"></span>
+                                <div className="relative h-full p-5 bg-white border-2 border-purple-500 rounded-lg">
+                                    <div className="flex items-center -mt-1">
+                                        <h3 className="my-2 ml-3 text-lg font-bold text-gray-800">Best Renting Experience in Pakistan</h3>
+                                    </div>
+                                    <p className="mt-3 mb-1 text-xs font-medium text-purple-500 uppercase">------------</p>
+                                    <p className="mb-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quas iusto quasi quibusdam excepturi accusantium dolores cupiditate praesentium alias at architecto assumenda, iste vitae ea beatae fugiat nihil perferendis ipsa?.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col w-full mb-5 sm:flex-row">
+                        <div className="w-full mb-10 sm:mb-0 sm:w-1/2">
+                            <div className="relative h-full ml-0 mr-0 sm:mr-10">
+                                <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-blue-400 rounded-lg"></span>
+                                <div className="relative h-full p-5 bg-white border-2 border-blue-400 rounded-lg">
+                                    <div className="flex items-center -mt-1">
+                                        <h3 className="my-2 ml-3 text-lg font-bold text-gray-800">Secure Payment  System</h3>
+                                    </div>
+                                    <p className="mt-3 mb-1 text-xs font-medium text-blue-400 uppercase">------------</p>
+                                    <p className="mb-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id harum laborum provident eum magni aliquam aspernatur molestiae dicta corporis rem qui quae suscipit at doloribus, quisquam ratione placeat impedit perferendis!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full mb-10 sm:mb-0 sm:w-1/2">
+                            <div className="relative h-full ml-0 mr-0 sm:mr-10">
+                                <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-yellow-400 rounded-lg"></span>
+                                <div className="relative h-full p-5 bg-white border-2 border-yellow-400 rounded-lg">
+                                    <div className="flex items-center -mt-1">
+                                        <h3 className="my-2 ml-3 text-lg font-bold text-gray-800">Best Variety of jewelary and Stitched Suits</h3>
+                                    </div>
+                                    <p className="mt-3 mb-1 text-xs font-medium text-yellow-400 uppercase">------------</p>
+                                    <p className="mb-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet error, repellat non sunt esse repudiandae, ut reiciendis quo fuga dicta officia labore laborum ullam hic cum voluptate, impedit vitae delectus.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full sm:w-1/2">
+                            <div className="relative h-full ml-0 md:mr-10">
+                                <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-green-500 rounded-lg"></span>
+                                <div className="relative h-full p-5 bg-white border-2 border-green-500 rounded-lg">
+                                    <div className="flex items-center -mt-1">
+                                        <h3 className="my-2 ml-3 text-lg font-bold text-gray-800">Database for controlling multiple requests</h3>
+                                    </div>
+                                    <p className="mt-3 mb-1 text-xs font-medium text-green-500 uppercase">------------</p>
+                                    <p className="mb-2 text-gray-600">Bot development frameworks were created as advanced software tools
+                                        that eliminate a large amount of manual work and accelerate the development process.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
